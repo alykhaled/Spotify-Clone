3903f6f921fc2cc736663f3422e2f97d0c4e919d
Aly Khaled <alykhaled2001@live.com>
2021-09-10 16:00:53 +0200

    Update Card

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './card.scss';
+
+export default function Card() {
+    return (
+        <div className="card">
+            <img src="https://newjams-images.scdn.co/v3/release-radar/ab6761610000e5eb3b2a1998ae37ecea4ab7c570/en/default" alt="pablo"/>
+            <p className="cardTitle">Release Radar</p>
+            <p className="cardDesc">fsdfsdfdsfdfsdfsdfdsfdfdfdfsdfsdfdsfdfdfsdfsdfdsfdfd</p> 
+        </div>
+    )
+}
