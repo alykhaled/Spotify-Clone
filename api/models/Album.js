697004768d1aa8fbaca8573c9c07d6347dbb779e
Aly Khaled <alykhaled2001@live.com>
2021-09-14 14:17:36 +0200

    Update schemas

--- api/models/Album.js
+++ api/models/Album.js
@@ -5,7 +5,8 @@ const AlbumScheme = new mongoose.Schema(
         name: {type: String, required: true},
         image: {type: String, default: "https://via.placeholder.com/250x250"},       
         link: {type: String, required: false},
-        artist: {type: mongoose.Schema.Types.ObjectId , ref: 'Artist',required: false}
+        artist: {type: mongoose.Schema.Types.ObjectId , ref: 'Artist',required: false},
+        year: {type: String, required: false},
     }
 )
 
--- api/routes/albums.js
+++ api/routes/albums.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const Album = require('../models/Album');
+const Artist = require('../models/Artist');
 const verify = require('../verifyToken');
+const mongoose = require('mongoose');
+
 
 //Create
 router.post("/",async (req,res) => {
@@ -9,16 +12,40 @@ router.post("/",async (req,res) => {
         image:req.body.image,
         link:req.body.link,
         artist: req.body.artist,
+        year: req.body.year
     });
 
     try 
     {
         const Album = await newAlbum.save();
+        const artist = await Artist.findById(Album.artist);
+        const artistAlbums = artist.albums;
+        artistAlbums.unshift(Album);
+        const artistNew = await Artist.findByIdAndUpdate(Album.artist, {albums: artistAlbums}, {new: true})
+        console.log(artistNew);
         res.status(200).send(Album);
     } 
     catch (error) 
     {
+        console.log(error);
         res.status(500).send(error);
     }
+});
+
+//GET
+router.get("/" ,async (req,res) => {
+    const query = req.query.artistId;
+    try 
+    {
+        let list = [];
+        list = await Album.aggregate([{ $match: {artist: mongoose.Types.ObjectId(query)} }]);
+        res.status(200).send(list);
+    } 
+    catch (error) 
+    {
+        console.log(error);
+        res.status(500).send(error);
+    }
+    
 });
 module.exports = router;
