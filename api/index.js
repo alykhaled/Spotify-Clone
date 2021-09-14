549daa86ca7e53c493b9bb2ab748a12fd1d3353a
Aly Khaled <alykhaled2001@live.com>
2021-09-14 06:33:41 +0200

    fix heroku bug

--- api/index.js
+++ api/index.js
@@ -26,6 +26,6 @@ app.get("/",(req,res) => {
     res.send("HELLO")
 })
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log('Server is running on port 8000');
 });
